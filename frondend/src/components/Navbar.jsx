6234ac73be4cd1de4755c29de53a2240302cb043
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-13 00:49:33 +0530

    Update Navbar.jsx

--- frondend/src/components/Navbar.jsx
+++ frondend/src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const Navbar = () => {
   }, []);
 
   return (
-    <div className="Navbar w-full h-[80px] px-[40px] py-[16px] flex flex-row items-center justify-between bg-white shadow-md fixed top-0 left-0 z-50">
+    <div className="Navbar w-full h-[80px] px-[40px] py-[16px] flex flex-row items-center justify-between bg-white fixed top-0 left-0 z-50">
       <div className="Logo w-[201px] h-[48px] flex flex-row items-center justify-between">
         <img src={Logo} alt="logo" className="w-[48px] h-[48px]" />
         <h1 className="w-[137px] h-[30px] text-[20px] text-center font-roboto font-medium text-[#65686C]">
@@ -55,7 +55,7 @@ const Navbar = () => {
         {sections.map((id) => (
           <button
             key={id}
-            className={`text-[24px] font-roboto font-medium px-4 py-2 transition-all duration-300 
+            className={`text-[24px] font-roboto font-medium cursor-pointer transition-all duration-300 
               ${activeSection === id ? "text-[#0866FF]" : "text-[#65686C]"}`}
             onClick={() => handleClickScroll(id)}
           >
@@ -69,7 +69,7 @@ const Navbar = () => {
         <h1 className="text-[16px] text-center font-roboto font-normal text-[#65686C]">
           Are you an Officer?
         </h1>
-        <button className="text-[16px] text-center font-roboto font-medium text-[#fff] px-[16px] py-[8px] rounded-[6px] bg-[#0866FF]">
+        <button className="text-[16px] text-center font-roboto font-medium text-[#fff] px-[16px] py-[8px] rounded-[6px] bg-[#0866FF] cursor-pointer">
           <Link to="/login">Login</Link>
         </button>
       </div>
