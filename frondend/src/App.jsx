6f7449c163e636ac72d2b601d0a6478717f8701e
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-13 00:20:57 +0530

    Create Page Routes

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home"
+import Login from "./pages/Login";
+import NotFound from "./pages/NotFound"
 
 function App() {
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/*" element={<NotFound/>}/>
+      </Routes>
+    </Router>
   )
 }
 
