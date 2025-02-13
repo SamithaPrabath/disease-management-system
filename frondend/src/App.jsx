50e86ca27d72b81242500f85c59e4ca5e1044e9c
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-13 09:44:56 +0530

    Update App.jsx

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"
-import Login from "./pages/Login";
+import Home from "./pages/HomePage"
+import Login from "./pages/LoginPage";
 import NotFound from "./pages/NotFound"
 
 function App() {
