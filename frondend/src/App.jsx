5ba36ebff18b6753105bc70aba7f3a60cda0fd1d
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-15 00:17:46 +0530

    Update App.jsx

--- frondend/src/App.jsx
+++ frondend/src/App.jsx
@@ -1,23 +1,47 @@
-import React from "react"
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import React, {useEffect} from "react"
+import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
+
 import Home from "./pages/HomePage"
 import Login from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound"
+import { connect } from 'react-redux';
 
-function App() {
+const  App = (props) => {
+
+  const navigate = useNavigate();
+
+  const response = props.AllLogins;
+
+  useEffect(() => {
+    if(response?.status == "200"){
+      sessionStorage.setItem("token", response?.data?.token);
+      navigate("/dashboard");
+    }else{
+      sessionStorage.removeItem("token");
+    }
+  }, [props.AllLogins]);
 
   return (
     <div className="font-roboto">
-      <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/login" element={<Login/>}/>
+        <Route path="/login" element={<Login />}/>
         <Route path="/home" element={<Home/>}/>
+        <Route
+            path="/dashboard"
+            element={ sessionStorage.getItem("token") != null ? <Dashboard /> : <Navigate to="/login" replace />}
+          />
         <Route path="/*" element={<NotFound/>}/>
       </Routes>
-    </Router>
     </div>
   )
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    AllLogins: state.allLogins,
+  };
+};
+
+export default connect(mapStateToProps)(App);
