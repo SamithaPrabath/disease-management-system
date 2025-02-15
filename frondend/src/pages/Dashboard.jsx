fbcd64f56d6acef5251b6183269e2f1685942a31
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-15 11:14:35 +0530

    Update Dashboard.jsx

--- frondend/src/pages/Dashboard.jsx
+++ frondend/src/pages/Dashboard.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
   return (
-    <div>
-      Hello Dashboard
-    </div>
+    <>
+      <Navbar />
+      <div className="Home w-full bg-[#F2F4F7] pt-[112px] pb-[32px] px-[32px] flex flex-col gap-[72px]">
+      </div>
+    </>
   );
 }
 
