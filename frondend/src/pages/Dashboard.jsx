739261462eb8d4ffbffc8717075570dd16a772ba
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-15 23:45:41 +0530

    Update Dashboard.jsx

--- frondend/src/pages/Dashboard.jsx
+++ frondend/src/pages/Dashboard.jsx
@@ -2,11 +2,10 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 
 const Dashboard = () => {
+  const Links = ["home", "Map"];
   return (
     <>
-      <Navbar />
-      <div className="Home w-full bg-[#F2F4F7] pt-[112px] pb-[32px] px-[32px] flex flex-col gap-[72px]">
-      </div>
+      <Navbar Sections={Links}/>
     </>
   );
 }
