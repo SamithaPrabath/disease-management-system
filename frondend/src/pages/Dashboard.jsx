eb1831d5aa5429d3a9432a200e8873deaa76d055
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-16 13:49:48 +0530

    Update Dashboard.jsx

--- frondend/src/pages/Dashboard.jsx
+++ frondend/src/pages/Dashboard.jsx
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Navbar from "../components/Navbar";
 
+import Home from '../components/epidemiology/Home';
+import Phi from '../components/epidemiology/Phi';
+import Moh from '../components/epidemiology/Moh'
+import Istitutes from '../components/epidemiology/Institutes'
+import Diseases from '../components/epidemiology/Didseaces'
+import Institutes from '../components/epidemiology/Institutes';
+
 const Dashboard = () => {
-  const Links = ["home", "Map"];
+  const Links = [
+    { id: "home", component: <Home /> },
+    { id: "phi", component: <Phi /> },
+    { id: "moh", component: <Moh/>},
+    {id: "institutes", component: <Institutes/>},
+    {id: "diseases", component: <Diseases/>}
+  ];
+
+  const [activeComponent, setActiveComponent] = useState(Links[0].id);
+
+  const handleNavClick = (id) => {
+    setActiveComponent(id);
+  };
+
+  const activeSection = Links.find((section) => section.id === activeComponent);
+
   return (
     <>
-      <Navbar Sections={Links}/>
+      <Navbar
+        Sections={Links}
+        onNavClick={handleNavClick}
+        activeId={activeComponent}
+      />
+      {/* Render the active component */}
+      <div className="p-4">{activeSection?.component}</div>
     </>
   );
 }
