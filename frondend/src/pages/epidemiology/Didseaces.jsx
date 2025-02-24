ab9887b63d29155d30cdc4abe7b5d0d2a43d418a
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-25 00:18:31 +0530

    Update Didseaces.jsx

--- frondend/src/pages/epidemiology/Didseaces.jsx
+++ frondend/src/pages/epidemiology/Didseaces.jsx
@@ -1,11 +1,91 @@
-import React from 'react';
+import React, { useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import AddDiseases from "../../components/epidemiology/AddDiseases";
+import RegisterTableCard from "../../components/RegisterTableCard";
+
+const initialData = [
+  {
+    registationNumber: "0001",
+    name: "John Doe",
+    email: "john.doe@email.com",
+    phoneNumber: "0764524589",
+  },
+  {
+    registationNumber: "0002",
+    name: "John Doe",
+    email: "john.doe@email.com",
+    phoneNumber: "0764524589",
+  },
+]
 
 const Didseaces = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleSearchChange = (e) =>
+    setSearchQuery(e.target.value.toLowerCase());
+
+    const tableData = [
+      {tableHeaders: ["Registation Number",
+      "Name",
+      "Email",
+      "Phone Number",
+      "Actions",],
+      tableData: initialData,
+      searchQuery: searchQuery,
+    }
+    ]
+
+    const handlePopUpOpen = () => {
+      setIsOpen(false);
+    };
+  
+    const handleBack = () => { 
+      setIsOpen(true);
+    };
+
   return (
-    <div  className='mt-[80px]'>
-      Diseases
+    <>
+      <div className="Phi w-full min-w-[870px] min-h-[500px] flex flex-col items-center justify-start gap-[32px]">
+      
+      {
+        isOpen ? (
+          <div className="w-full min-h-[200px] bg-white flex flex-col p-[32px] gap-[24px]">
+
+        <div className="w-full flex flex-row items-center justify-between">
+          {/* Heading */}
+          <h2 className="w-full text-[32px] font-medium text-[#080809] text-left">
+            Didseaces
+          </h2>
+
+          <div className="flex flex-row gap-3">
+          <button className="w-[150px] text-white text-[16px] font-medium rounded-[8px] bg-[#0866FF] p-[8px] cursor-pointer"
+          onClick={handlePopUpOpen}
+          >
+            Add Didseaces
+          </button>
+          <div className="relative">
+            <input
+              className="bg-[#E2E5E9] w-[250px] h-[50px] rounded-[8px] px-[16px] py-[14px] text-black placeholder-gray-600 focus:outline-none"
+              type="text"
+              placeholder="Search"
+              value={searchQuery}
+              onChange={handleSearchChange}
+            />
+            <CiSearch className="absolute right-[16px] top-1/2 transform -translate-y-1/2 text-gray-500 text-xl" />
+          </div>
+          </div>
+        </div>
+
+        <RegisterTableCard tableData={tableData}/>
+      </div>
+
+        ) : (<AddDiseases handleBack={handleBack}/>)
+      }
+
     </div>
+    </>
   );
-}
+};
 
 export default Didseaces;
