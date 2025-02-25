d3239516aaf16c719c855253a409330de5940f42
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-25 21:00:49 +0530

    Update index.js

--- frondend/src/redux/reducers/index.js
+++ frondend/src/redux/reducers/index.js
@@ -1,11 +1,13 @@
 import {combineReducers} from 'redux';
 import AllLoginReducer from './login-reducer';
 import AllPopupReducer from './popup-reducer'
+import AllViewEditReducer from './viewedit-reducer'
 
 const rootReducer = combineReducers(
     {
         allLogins: AllLoginReducer,
         allPopup: AllPopupReducer,
+        allViewEditReducer: AllViewEditReducer,
     }
 )
 
