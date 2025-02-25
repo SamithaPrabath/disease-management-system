0b588dac055d42ec4e4b5ec8474d13aade25a9a9
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-25 21:00:52 +0530

    Create viewEditAction.js

---
+++ frondend/src/redux/actions/viewEditAction.js
@@ -0,0 +1,18 @@
+import { VIEW_EDIT_BUTTON_CLICKED } from "./types";
+import { VIEW_EDIT_CLOSE_BUTTON_CLICKED } from "./types";
+
+
+export const viewEdit = () => {
+      return{
+        type: VIEW_EDIT_BUTTON_CLICKED,
+        payload: [true],
+      }
+  };
+
+  export const closeViewEdit = (values) => {
+
+    return{
+      type: VIEW_EDIT_CLOSE_BUTTON_CLICKED,
+      payload: [false,values],
+    }
+};
