722b632c5ecf171cc706acdd723ebe904c6837d0
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-25 21:01:43 +0530

    Update loginApi.js

--- frondend/src/api/loginApi.js
+++ frondend/src/api/loginApi.js
@@ -7,6 +7,7 @@ const testUser = { username: "test", password: "123" };
 const testResponse = {
     status: 200,
     data: {
+      id: "001",
       token: "abc1234",
       name: "John Doe",
       role: "admin", 
