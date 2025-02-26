535844c71943ef41c3ea549c2a354f073fbdab75
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-26 15:46:13 +0530

    Create Events.jsx

---
+++ frondend/src/pages/phi/Events.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Events = () => {
+  return (
+    <div className="Events w-full min-w-[870px] min-h-[500px] bg-white flex flex-col items-center justify-center px-[32px] py-[48px] gap-[32px]">
+      events
+    </div>
+  );
+}
+
+export default Events;
