6fad8ceefb1b4187a6757473e44e161f27f093ca
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-26 15:46:39 +0530

    Update loginApi.js

--- frondend/src/api/loginApi.js
+++ frondend/src/api/loginApi.js
@@ -10,7 +10,7 @@ const testResponse = {
       id: "001",
       token: "abc1234",
       name: "John Doe",
-      role: "admin", 
+      role: "doctor", 
       regNo: "001",
       gender: "Male",
       age: "35",
