de1686a0726ff76c2e1dec8d794b857a7d86ed4e
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-27 01:03:32 +0530

    Update Events.jsx

--- frondend/src/components/Public/Events.jsx
+++ frondend/src/components/Public/Events.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getAllEvents } from "../../api/eventsApi";
 
 const Events = () => {
+  const [allEvents, setAllEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchEvents = async () => {
+      const events = await getAllEvents();
+      setAllEvents(events);
+    };
+    fetchEvents();
+  }, []);
+
   return (
-    <div id="Events" className="w-full min-w-[870px] min-h-[500px] bg-white flex flex-col items-center justify-center px-[32px] py-[48px] gap-[32px]">
+    <div
+      id="Events"
+      className="w-full min-w-[870px] min-h-[500px] bg-white flex flex-col items-center justify-center px-[32px] py-[48px] gap-[32px]"
+    >
       <div className="Header">
         <h2 className="text-[32px] font-medium text-[#080809] text-center">
           Public Health Events & Announcements
@@ -14,47 +28,33 @@ const Events = () => {
       </div>
 
       <div className="w-full flex flex-wrap gap-[16px] items-center justify-center">
-
-        <div className="Event-Card w-[316px] bg-white rounded-[8px] drop-shadow-md shadow-[#E2E5E9] mb-[32px]">
-          <img src="" alt="eventImage" className="w-full h-[188px]"/>
+        {allEvents.map((event) => (
+          <div
+          className="Event-Card w-[316px] bg-white rounded-[8px] drop-shadow-md shadow-[#E2E5E9] mb-[32px]"
+          key={event.id}
+        >
+          <img
+            src={event.image}
+            alt="eventImage"
+            className="w-full h-[188px] object-cover"
+          />
           <div className="p-[16px]">
-            <p className="Date text-[16px] text-[#65686C] fort-nomal">Sat, Feb 15 at 1PM</p>
-            <h1 className="text-[20px] text-[#080809] font-medium">Health Check-Up Camp</h1>
-            <h2 className="text-[14px] text-[#080809] font-nomal">Maharagama Hospital</h2>
-              <p className="text-[14px] text-[#65686C] font-nomal">Join our free health check-up camp for expert consultations, diagnostics, and wellness tips. Prioritize your health</p>
+            <p className="Date text-[16px] text-[#65686C] font-normal flex gap-2">
+            <span>{event.startDate}</span>
+  <span>{event.startTime}</span>
+            </p>
+            <h1 className="text-[20px] text-[#080809] font-medium">
+              {event.eventName}
+            </h1>
+            <h2 className="text-[14px] text-[#080809] font-normal">
+              {event.location}
+            </h2>
+            <p className="text-[14px] text-[#65686C] font-normal">
+              {event.description}
+            </p>
           </div>
         </div>
-
-        <div className="Event-Card w-[316px] bg-white rounded-[8px] drop-shadow-md shadow-[#E2E5E9] mb-[32px]">
-          <img src="" alt="eventImage" className="w-full h-[188px]"/>
-          <div className="p-[16px]">
-            <p className="Date text-[16px] text-[#65686C] fort-nomal">Sat, Feb 15 at 1PM</p>
-            <h1 className="text-[20px] text-[#080809] font-medium">Health Check-Up Camp</h1>
-            <h2 className="text-[14px] text-[#080809] font-nomal">Maharagama Hospital</h2>
-              <p className="text-[14px] text-[#65686C] font-nomal">Join our free health check-up camp for expert consultations, diagnostics, and wellness tips. Prioritize your health</p>
-          </div>
-        </div>
-
-        <div className="Event-Card w-[316px] bg-white rounded-[8px] drop-shadow-md shadow-[#E2E5E9] mb-[32px]">
-          <img src="" alt="eventImage" className="w-full h-[188px]"/>
-          <div className="p-[16px]">
-            <p className="Date text-[16px] text-[#65686C] fort-nomal">Sat, Feb 15 at 1PM</p>
-            <h1 className="text-[20px] text-[#080809] font-medium">Health Check-Up Camp</h1>
-            <h2 className="text-[14px] text-[#080809] font-nomal">Maharagama Hospital</h2>
-              <p className="text-[14px] text-[#65686C] font-nomal">Join our free health check-up camp for expert consultations, diagnostics, and wellness tips. Prioritize your health</p>
-          </div>
-        </div>
-
-        <div className="Event-Card w-[316px] bg-white rounded-[8px] drop-shadow-md shadow-[#E2E5E9] mb-[32px]">
-          <img src="" alt="eventImage" className="w-full h-[188px]"/>
-          <div className="p-[16px]">
-            <p className="Date text-[16px] text-[#65686C] fort-nomal">Sat, Feb 15 at 1PM</p>
-            <h1 className="text-[20px] text-[#080809] font-medium">Health Check-Up Camp</h1>
-            <h2 className="text-[14px] text-[#080809] font-nomal">Maharagama Hospital</h2>
-              <p className="text-[14px] text-[#65686C] font-nomal">Join our free health check-up camp for expert consultations, diagnostics, and wellness tips. Prioritize your health</p>
-          </div>
-        </div>
-
+        ))}
       </div>
     </div>
   );
