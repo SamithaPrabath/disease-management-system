9e02721bd395e5c1dc0db81ea49ab8a6794852e2
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-26 15:45:48 +0530

    Update popup-reducer.js

--- frondend/src/redux/reducers/popup-reducer.js
+++ frondend/src/redux/reducers/popup-reducer.js
@@ -1,14 +1,15 @@
-import { POPUP_VIEW_BUTTON_CLICKED } from "../actions/types";
-import { POPUP_CLOSE_BUTTON_CLICKED } from "../actions/types";
+import {
+  POPUP_VIEW_BUTTON_CLICKED,
+  POPUP_CLOSE_BUTTON_CLICKED,
+} from "../actions/types";
 
-export default function(state=null, action){
-    switch(action.type){
-        case POPUP_VIEW_BUTTON_CLICKED:
-            return action.payload;
-            break;
-        case POPUP_CLOSE_BUTTON_CLICKED:
-            return action.payload;
-            break;
-    }
-    return state
+export default function (state = null, action) {
+  switch (action.type) {
+    case POPUP_VIEW_BUTTON_CLICKED:
+      return action.payload;
+      break;
+    case POPUP_CLOSE_BUTTON_CLICKED:
+      return action.payload;
+  }
+  return state;
 }
