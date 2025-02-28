570e7d6d30f8251ad7d0fa755243b97542ef4953
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-03-01 01:56:45 +0530

    Update loginApi.js

--- frondend/src/api/loginApi.js
+++ frondend/src/api/loginApi.js
@@ -4,33 +4,33 @@ const BASE_URL = import.meta.env.VITE_BASE_URL;
 const IS_BACKEND = import.meta.env.VITE_IS_BACKEND;
 
 const testUser = { username: "test", password: "123" };
-const testResponse = {
-    status: 200,
-    data: {
-      userId: "001",
-      token: "abc1234",
-      role: "doctor", 
-      userTypeId: "D001",
-    },
-    message: "Ok",
-  };
+export const testResponse = [
+  {
+    userId: "001",
+    token: "abc1234",
+    role: "doctor",
+    userTypeId: "D001",
+  },
+];
 
-  const errorResponse = {
-    status: 401,
-    error:"Unauthorized Access",
-    message: "Invalid Username or Password"
-  };
+const errorResponse = {
+  status: 401,
+  error: "Unauthorized Access",
+  message: "Invalid Username or Password",
+};
 
 export const handleLogin = async (user) => {
   try {
     if (IS_BACKEND) {
-      if (user.username === testUser.username && user.password === testUser.password) {
-        return testResponse;
+      if (
+        user.username === testUser.username &&
+        user.password === testUser.password
+      ) {
+        return { status: 200, data: testResponse?.[0], message: "Ok" };
       } else {
         return errorResponse;
       }
     } else {
-
       const response = await axios.post(`${BASE_URL}/login`, {
         username: user.username,
         password: user.password,
