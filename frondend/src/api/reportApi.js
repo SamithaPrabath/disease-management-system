b61beb2ac9d4d2538293930c6c991c9075cc00e2
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-28 21:31:33 +0530

    Create reportApi.js

---
+++ frondend/src/api/reportApi.js
@@ -0,0 +1,51 @@
+import axios from "axios";
+
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const IS_BACKEND = import.meta.env.VITE_IS_BACKEND;
+
+export const reportResponse = [
+  {
+    id: "001",
+    ethnicGroup: "Sinhalese",
+    dischargeDate: "2023-10-20",
+    isolationDateFrom: "2023-10-16",
+    isolationDateTo: "2023-10-20",
+    movementHistory: "No recent travel",
+    isolationStatus: "Home",
+    outcome: "Recovered",
+    labResults: "Positive for Dengue IgM",
+    phiRemarks: "Patient responded well to treatment",
+    file: null,
+  },
+  {
+    id: "002",
+    ethnicGroup: "Sinhalese",
+    dischargeDate: "2023-10-20",
+    isolationDateFrom: "2023-10-16",
+    isolationDateTo: "2023-10-20",
+    movementHistory: "No recent travel",
+    isolationStatus: "Home",
+    outcome: "Recovered",
+    labResults: "Positive for Dengue IgM",
+    phiRemarks: "Patient responded well to treatment",
+    file: null,
+  },
+];
+
+export const addReport = async (report) => {
+  try {
+    if (IS_BACKEND) {
+      return { message: "Report add successfully", case: report };
+    } else {
+      // Make an API call to update the case
+      const response = await axios.put(
+        `${BASE_URL}/updateCase/${report.caseId}`,
+        report
+      );
+      return response.data; // Return the data received from the API
+    }
+  } catch (error) {
+    console.error("Error updating case:", error);
+    throw error; // Throw the error if the request fails
+  }
+};
