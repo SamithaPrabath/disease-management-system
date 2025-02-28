7de96bcb5efc6d587b59ebfc26e7788b54ad416a
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-02-28 21:30:21 +0530

    Update Home.jsx

--- frondend/src/pages/idu/Home.jsx
+++ frondend/src/pages/idu/Home.jsx
@@ -1,28 +1,44 @@
 import React, { useState, useEffect } from "react";
-import AllCasesTable from "../../components/AllCasesTable";
-import { getAllCases } from "../../api/allCasesApi";
 import { CiSearch } from "react-icons/ci";
+import TableCard from "../../components/phi/TableCard";
+import SingleCaseView from "../../components/SingleCaseView";
+import { connect } from "react-redux";
+import Report from "../../components/phi/Report";
+import { getAllCases } from "../../api/allCasesApi";
 
-const Home = () => {
-  const [allCases, setAllCases] = useState([]);
+const Home = (props) => {
   const [searchQuery, setSearchQuery] = useState("");
+  const [isViewReport, setIsViewReport] = useState(true);
+  const [allCasesData, setAllCasesData] = useState([]);
+  const [patientId, setPatientId] = useState("")
 
-  const handleSearchChange = (e) =>
-    setSearchQuery(e.target.value.toLowerCase());
+  const [isViewSingleCase, setIsViewSingleCase] = useState(false);
 
-  // Fetch data when the component mounts
   useEffect(() => {
+
+    setIsViewReport(props.ViewReport?.[0]);
+
+    // Fetch all cases data
     const fetchData = async () => {
       try {
         const data = await getAllCases();
-        setAllCases(data);
+        setAllCasesData(data);
       } catch (error) {
-        console.error("Failed to fetch data:", error);
+        console.error("Failed to fetch cases data:", error);
       }
     };
 
     fetchData();
-  }, []);
+  }, [props.ViewReport]);
+
+
+  useEffect(() => {
+    setIsViewSingleCase(props.ViewsSingleCase?.[0]);
+    setPatientId(props.ViewsSingleCase?.[1]);
+  }, [props.ViewsSingleCase])
+
+  const handleSearchChange = (e) =>
+    setSearchQuery(e.target.value.toLowerCase());
 
   const tableData = [
     {
@@ -36,23 +52,28 @@ const Home = () => {
         "Case Status",
         "Actions",
       ],
-      tableData: allCases,
+      tableData: allCasesData,
       searchQuery: searchQuery,
+      handlePopUpOpen: null,
     },
   ];
 
   return (
     <>
-      <div className="Home w-full min-w-[870px] min-h-[500px] bg-white flex flex-col items-center justify-center px-[32px] py-[48px] gap-[32px]">
-        {/* Render the active component */}
+      {
+        isViewSingleCase && !isViewReport ? (<SingleCaseView patientId = {patientId}/>) : (
+          <>
+      {isViewReport ? (
+        <Report />
+      ) : (
+        <div className="Home w-full min-w-[870px] min-h-[500px] bg-white flex flex-col items-center justify-center px-[32px] py-[48px] gap-[32px]">
+          <div className="w-full flex flex-row items-center justify-between">
+            {/* Heading */}
+            <h2 className="w-full text-[32px] font-medium text-[#080809] text-left">
+              Notifications of a communicable disease
+            </h2>
 
-        <div className="w-full flex flex-row items-center justify-between">
-          {/* Heading */}
-          <h2 className="w-full text-[32px] font-medium text-[#080809] text-left">
-            Notifications of a communicable disease
-          </h2>
-
-          <div className="flex flex-row gap-3">
+            {/* Search Input with Icon */}
             <div className="relative">
               <input
                 className="bg-[#E2E5E9] w-[250px] h-[50px] rounded-[8px] px-[16px] py-[14px] text-black placeholder-gray-600 focus:outline-none"
@@ -64,11 +85,22 @@ const Home = () => {
               <CiSearch className="absolute right-[16px] top-1/2 transform -translate-y-1/2 text-gray-500 text-xl" />
             </div>
           </div>
+
+          <TableCard tableData={tableData} />
         </div>
-        <AllCasesTable tableData={tableData} />
-      </div>
+      )}
+    </>
+        )
+      }
     </>
   );
 };
 
-export default Home;
+const mapStateToProps = (state) => {
+  return {
+    ViewReport: state.viewReportReducer,
+    ViewsSingleCase: state.viewsSingleCase,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
