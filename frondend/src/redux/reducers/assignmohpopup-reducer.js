956569117ecbb805c110cc63b68805ff096d2215
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-03-02 20:50:06 +0530

    Create assignmohpopup-reducer.js

---
+++ frondend/src/redux/reducers/assignmohpopup-reducer.js
@@ -0,0 +1,16 @@
+import {
+    VIEW_ASSIGN_MOH_BUTTON_CLICKED, VIEW_ASSIGN_MOH_BUTTON_CLOSE_BUTTON_CLICKED
+  } from "../actions/types";
+
+  export default function (state = null, action) {
+    switch (action.type) {
+      case VIEW_ASSIGN_MOH_BUTTON_CLICKED:
+        return action.payload;
+        break;
+      case VIEW_ASSIGN_MOH_BUTTON_CLOSE_BUTTON_CLICKED:
+        return action.payload;
+        break;
+    }
+    return state;
+  }
+  
