d8e5cbb04cb9cd59869f3b28ea324492f16c64f4
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-03-03 23:44:16 +0530

    Update eventSchema.js

--- frondend/src/yupSchema/eventSchema.js
+++ frondend/src/yupSchema/eventSchema.js
@@ -2,26 +2,14 @@ import * as Yup from "yup";
 
 export const eventSchema = Yup.object().shape({
   eventName: Yup.string()
-    .required("Event Name is required")
-    .min(3, "Event Name must be at least 3 characters"),
+    .required("Event Name is required"),
   startDate: Yup.date()
     .required("Start Date is required")
     .min(new Date(), "Start Date must be in the future"),
   startTime: Yup.string()
     .required("Start Time is required"),
   location: Yup.string()
-    .required("Location is required")
-    .min(5, "Location must be at least 5 characters"),
-  details: Yup.string()
+    .required("Location is required"),
+    description: Yup.string()
     .optional(),
-  photo: Yup.mixed()
-    .nullable()
-    .test("fileSize", "File size is too large", (value) => {
-      if (!value) return true; // No file is acceptable
-      return value.size <= 5 * 1024 * 1024; // 5MB limit
-    })
-    .test("fileType", "Unsupported file type", (value) => {
-      if (!value) return true; // No file is acceptable
-      return ["image/jpeg", "image/png", "image/gif"].includes(value.type);
-    }),
 });
