5e02be4c899d79556fb65addf84358c22555fa70
Julach Earzan <114212741+julach-erazan@users.noreply.github.com>
2025-03-03 11:31:29 +0530

    Update UserTableCard.jsx

--- frondend/src/components/UserTableCard.jsx
+++ frondend/src/components/UserTableCard.jsx
@@ -79,9 +79,9 @@ const UserTableCard = ({ tableData, closeViewEdit }) => {
                 <tbody className="divide-y divide-gray-200 bg-white">
                   {data.map((user) => (
                     <tr key={user.id} className="hover:bg-gray-50">
-                      {user.diseaseCode ? (
+                      {user.id ? (
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-[#080809] sm:pl-6">
-                          {user.diseaseCode}
+                          {user.id}
                         </td>
                       ) : null}
 
@@ -100,12 +100,6 @@ const UserTableCard = ({ tableData, closeViewEdit }) => {
                       {user.modeOfTransmission ? (
                         <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-[#080809] sm:pl-6">
                           {user.modeOfTransmission}
-                        </td>
-                      ) : null}
-
-                      {user.description ? (
-                        <td className="whitespace-wrap w-[200px] py-4 pl-4 pr-3 text-sm font-medium text-[#080809] sm:pl-6">
-                          {user.description}
                         </td>
                       ) : null}
 
