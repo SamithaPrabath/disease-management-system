e394c19bab910a5e0765b1a5df3c00821ee98f8b
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-03-22 13:56:47 +0530

    Update Navbar: add user name and role in navbr

--- frondend/src/components/Navbar.jsx
+++ frondend/src/components/Navbar.jsx
@@ -3,14 +3,29 @@ import { Link, useLocation, useNavigate } from "react-router-dom";
 import Logo from "../assets/logo.png";
 import { IoNotifications } from "react-icons/io5";
 import { HiUser } from "react-icons/hi2";
+import { connect } from "react-redux";
 
-const Navbar = ({ Sections, onNavClick, activeId }) => {
+const Navbar = ({ AllLogins, Sections, onNavClick, activeId }) => {
   const [activeSection, setActiveSection] = useState("Home");
   const [isDropdownVisible, setIsDropdownVisible] = useState(false); // State for user dropdown visibility
   const [isNotificationDropdownVisible, setIsNotificationDropdownVisible] =
     useState(false); // State for notification dropdown visibility
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [userName, setUserName] = useState("Null");
+  const [userRole, setUserRole] = useState("Null");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      if (AllLogins?.data) {
+        setUserName(AllLogins.data.username);
+        setUserRole(AllLogins.data.role);
+      }
+    };
+
+    fetchData();
+  }, [AllLogins]);
 
   // Mock notifications
   const mockNotifications = [
@@ -127,7 +142,7 @@ const Navbar = ({ Sections, onNavClick, activeId }) => {
               </button>
             ))}
           </ul>
-          <div className="w-[100px] h-[40px] flex flex-row items-center gap-[16px] relative">
+          <div className="min-w-[100px] h-[40px] flex flex-row items-center gap-[16px] relative">
             {/* Notification Button */}
             <div className="relative">
               <div className="relative">
@@ -187,6 +202,18 @@ const Navbar = ({ Sections, onNavClick, activeId }) => {
                 </div>
               )}
             </div>
+            <div className="flex flex-col items-start justify-center">
+              <div>
+                <p className="text-[14px] text-gray-500 font-bold">
+                  Hi<span className="text-[#0866FF]">, </span>
+                  {userName}
+                </p>
+              </div>
+              <p className="text-gray-400 flex flex-row items-center justify-between text-[10px] uppercase gap-1">
+                <div className="active w-[10px] h-[10px] bg-green-500 rounded-[50%]" />
+                {userRole}
+              </p>
+            </div>
           </div>
         </>
       )}
@@ -194,4 +221,10 @@ const Navbar = ({ Sections, onNavClick, activeId }) => {
   );
 };
 
-export default Navbar;
+const mapStateToProps = (state) => {
+  return {
+    AllLogins: state.allLogins,
+  };
+};
+
+export default connect(mapStateToProps, null)(Navbar);
