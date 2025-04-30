bf090992b6cdecdf48b0db5a1a121b1866a685f0
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-05-01 02:00:42 +0530

    Delete unnecessary files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import React, {useEffect} from "react"
+import React, { useEffect } from "react";
 import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 
-import Home from "./pages/PublicPage"
+import Home from "./pages/PublicPage";
 import Login from "./pages/LoginPage";
 
 import DashboardEpi from "./pages/epidemiology/Dashboard";
@@ -10,16 +10,17 @@ import DashboardDoc from "./pages/doctor/Dashboard";
 import DashboardIdu from "./pages/idu/Dashboard";
 import DashboardMoh from "./pages/moh/Dashboard";
 
-import NotFound from "./pages/NotFound"
-import { connect } from 'react-redux';
-import {encryptAndStoreToken, decryptAndRetrieveToken, isTokenValid} from "./Encryption/encrypt"
-import SingleCaseView from "./pages/SingleCaseView";
+import NotFound from "./pages/NotFound";
+import { connect } from "react-redux";
+import {
+  encryptAndStoreToken,
+  decryptAndRetrieveToken,
+  isTokenValid,
+} from "./Encryption/encrypt";
 
-const  App = (props) => {
-
+const App = (props) => {
   const navigate = useNavigate();
   const response = props.AllLogins;
-
 
   useEffect(() => {
     if (response?.status == "200") {
@@ -34,30 +35,36 @@ const  App = (props) => {
   return (
     <div className="font-roboto">
       <Routes>
-      <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<Navigate to="/home" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route
           path="/dashboard"
           element={
             decryptAndRetrieveToken() && isTokenValid() ? (
-              response?.data?.role === "admin" ? <DashboardEpi />
-              : response?.data?.role === "phi" ?<DashboardPhi /> : 
-              response?.data?.role === "doctor" ? <DashboardDoc/> : 
-              response?.data?.role === "idu" ? <DashboardIdu/> :
-              response?.data?.role === "moh" ? <DashboardMoh/> :
-              <Navigate to="/login" replace />
+              response?.data?.role === "admin" ? (
+                <DashboardEpi />
+              ) : response?.data?.role === "phi" ? (
+                <DashboardPhi />
+              ) : response?.data?.role === "doctor" ? (
+                <DashboardDoc />
+              ) : response?.data?.role === "idu" ? (
+                <DashboardIdu />
+              ) : response?.data?.role === "moh" ? (
+                <DashboardMoh />
+              ) : (
+                <Navigate to="/login" replace />
+              )
             ) : (
               <Navigate to="/login" replace />
             )
           }
         />
-        <Route path="/single-case-view/:caseId" element={<SingleCaseView />} />
         <Route path="/*" element={<NotFound />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
