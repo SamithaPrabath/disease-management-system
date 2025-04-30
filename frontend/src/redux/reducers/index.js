074655c8e28504c52242b4e35a6dce16c2443a3f
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-05-01 02:17:19 +0530

    Update index.js

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,5 +1,6 @@
 import {combineReducers} from 'redux';
 import AllLoginReducer from './login-reducer';
+import AllResetPasswordReducer from "./resetPassword-reducer"
 import AllPopupReducer from './popup-reducer'
 import AllViewEditReducer from './viewedit-reducer'
 import ViewReportReducer from './viewreport-reducer'
@@ -13,6 +14,7 @@ import ViewEditEvent from './viewEditEvent-reducer';
 const rootReducer = combineReducers(
     {
         allLogins: AllLoginReducer,
+        allResetPasswordReducer: AllResetPasswordReducer,
         allPopup: AllPopupReducer,
         allViewEditReducer: AllViewEditReducer,
         viewReportReducer: ViewReportReducer,
