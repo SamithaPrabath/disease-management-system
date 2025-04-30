a3e064d154b2fb4ae2de2d2fea7bd083476d86d7
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-05-01 02:17:16 +0530

    Create resetPassword-reducer.js

---
+++ frontend/src/redux/reducers/resetPassword-reducer.js
@@ -0,0 +1,10 @@
+import { RESET_PASSWORD_BUTTON_CLICKED } from "../actions/types";
+
+export default function(state=null, action){
+    switch(action.type){
+        case RESET_PASSWORD_BUTTON_CLICKED:
+            return action.payload;
+            break;
+    }
+    return state
+}
