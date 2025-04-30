d9c8cd5a710bdb92b0aaf20d401be34f82b599e3
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-05-01 01:51:16 +0530

    Update popUpAction.js

--- frontend/src/redux/actions/popUpAction.js
+++ frontend/src/redux/actions/popUpAction.js
@@ -1,16 +1,22 @@
 import { POPUP_VIEW_BUTTON_CLICKED, POPUP_CLOSE_BUTTON_CLICKED } from "./types";
 
-export const openPopUp = () => {
-      return{
-        type: POPUP_VIEW_BUTTON_CLICKED,
-        payload: true,
-      }
-  };
+export const openPopUp = (cardId) => {
+  return {
+    type: POPUP_VIEW_BUTTON_CLICKED,
+    payload: {
+      isOpen: true,
+      cardId: cardId
+    },
+  }
+};
 
-  export const closePopUp = () => {
-    return{
-      type: POPUP_CLOSE_BUTTON_CLICKED,
-      payload: false,
-    }
+export const closePopUp = () => {
+  return {
+    type: POPUP_CLOSE_BUTTON_CLICKED,
+    payload: {
+      isOpen: false,
+      cardId: null
+    },
+  }
 };
 
