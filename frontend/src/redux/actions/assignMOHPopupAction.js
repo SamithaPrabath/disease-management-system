e0eb6a540f7f55e8732bb75751fabd1f8d2e724f
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-05-02 21:21:07 +0530

    Update assignMOHPopupAction.js

--- frontend/src/redux/actions/assignMOHPopupAction.js
+++ frontend/src/redux/actions/assignMOHPopupAction.js
@@ -1,15 +1,15 @@
 import { VIEW_ASSIGN_MOH_BUTTON_CLICKED, VIEW_ASSIGN_MOH_BUTTON_CLOSE_BUTTON_CLICKED } from "./types";
 
-export const viewAssignMOHPopUp = () => {
-    return{
-      type: VIEW_ASSIGN_MOH_BUTTON_CLICKED,
-      payload: true,
-    }
-  };
-  
-  export const closeAssignMOHPopUp = () => {
-  return{
+export const viewAssignMOHPopUp = (value) => {
+  return {
+    type: VIEW_ASSIGN_MOH_BUTTON_CLICKED,
+    payload: [true, value],
+  }
+};
+
+export const closeAssignMOHPopUp = () => {
+  return {
     type: VIEW_ASSIGN_MOH_BUTTON_CLOSE_BUTTON_CLICKED,
     payload: false,
   }
-  };
+};
