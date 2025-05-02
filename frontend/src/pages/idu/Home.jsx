8ae91485e26363af1c067737bcee2c1872d35e25
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-05-02 21:21:01 +0530

    Update Home.jsx

--- frontend/src/pages/idu/Home.jsx
+++ frontend/src/pages/idu/Home.jsx
@@ -12,12 +12,11 @@ const Home = (props) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [isViewReport, setIsViewReport] = useState(true);
   const [allCasesData, setAllCasesData] = useState([]);
-  const [patientId, setPatientId] = useState("")
+  const [patientId, setPatientId] = useState("");
 
   const [isViewSingleCase, setIsViewSingleCase] = useState(false);
 
   useEffect(() => {
-
     setIsViewReport(props.ViewReport?.[0]);
 
     // Fetch all cases data
@@ -33,11 +32,10 @@ const Home = (props) => {
     fetchData();
   }, [props.ViewReport]);
 
-
   useEffect(() => {
     setIsViewSingleCase(props.ViewsSingleCase?.[0]);
     setPatientId(props.ViewsSingleCase?.[1]);
-  }, [props.ViewsSingleCase])
+  }, [props.ViewsSingleCase]);
 
   const handleSearchChange = (e) =>
     setSearchQuery(e.target.value.toLowerCase());
@@ -62,40 +60,40 @@ const Home = (props) => {
 
   return (
     <>
-      {
-        isViewSingleCase && !isViewReport ? (<SingleCaseView patientId = {patientId}/>) : (
-          <>
-      {isViewReport ? (
-        <Report />
+      {isViewSingleCase && !isViewReport ? (
+        <SingleCaseView patientId={patientId} />
       ) : (
-        <div className="Home w-full min-w-[870px] min-h-[500px] bg-white flex flex-col items-center justify-center px-[32px] py-[48px] gap-[32px]">
-          <div className="w-full flex flex-row items-center justify-between">
-            {/* Heading */}
-            <h2 className="w-full text-[32px] font-medium text-[#080809] text-left">
-            Notifiable Disease Notifications
-            </h2>
+        <>
+          {isViewReport ? (
+            <Report />
+          ) : (
+            <div className="Home w-full min-w-[870px] min-h-[500px] bg-white flex flex-col items-center justify-center px-[32px] py-[48px] gap-[32px]">
+              <div className="w-full flex flex-row items-center justify-between">
+                {/* Heading */}
+                <h2 className="w-full text-[32px] font-medium text-[#080809] text-left">
+                  Notifiable Disease Notifications
+                </h2>
 
-            {/* Search Input with Icon */}
-            <div className="relative">
-              <input
-                className="bg-[#E2E5E9] w-[250px] h-[50px] rounded-[8px] px-[16px] py-[14px] text-black placeholder-gray-600 focus:outline-none"
-                type="text"
-                placeholder="Search By Name"
-                value={searchQuery}
-                onChange={handleSearchChange}
-              />
-              <CiSearch className="absolute right-[16px] top-1/2 transform -translate-y-1/2 text-gray-500 text-xl" />
+                {/* Search Input with Icon */}
+                <div className="relative">
+                  <input
+                    className="bg-[#E2E5E9] w-[250px] h-[50px] rounded-[8px] px-[16px] py-[14px] text-black placeholder-gray-600 focus:outline-none"
+                    type="text"
+                    placeholder="Search By Name"
+                    value={searchQuery}
+                    onChange={handleSearchChange}
+                  />
+                  <CiSearch className="absolute right-[16px] top-1/2 transform -translate-y-1/2 text-gray-500 text-xl" />
+                </div>
+              </div>
+
+              <TableCard tableData={tableData} />
             </div>
-          </div>
-
-          <TableCard tableData={tableData} />
-        </div>
+          )}
+        </>
       )}
-    </>
-        )
-      }
-      <AssignPHIPopup/>
-      <AssignMOHPopup/>
+      <AssignPHIPopup />
+      <AssignMOHPopup />
     </>
   );
 };
