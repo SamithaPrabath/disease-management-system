b768eb6a978e56f33a729c5378a3e7fd55d0762b
julach-x4digital <114212741+julach-erazan@users.noreply.github.com>
2025-05-02 17:22:14 +0530

    validate form and update submitting data

--- frontend/src/components/ConfirmCasePopup.jsx
+++ frontend/src/components/ConfirmCasePopup.jsx
@@ -5,17 +5,24 @@ import { closeVIewConfirmPopUp } from "../redux/actions/confirmCasePopUpAction";
 import { message } from "antd";
 import { confirmCase } from "../api/allCasesApi";
 import { useNavigate } from "react-router-dom";
+import { Formik, Form, Field } from "formik";
+import * as Yup from "yup";
+
+const validationSchema = Yup.object().shape({
+  natureOfConfirmation: Yup.string().required(
+    "Please select nature of confirmation"
+  ),
+});
+
 const ConfirmCasePopup = ({
   AllLogins,
   ConfirmPopUp,
   closeVIewConfirmPopUp,
 }) => {
   const [messageApi, contextHolder] = message.useMessage();
-  const [selectedOption, setSelectedOption] = useState("");
   const [remarks, setRemarks] = useState("");
   const [userTypeId, setUserTypeId] = useState("");
   const navigate = useNavigate();
-
   const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
@@ -23,28 +30,25 @@ const ConfirmCasePopup = ({
   }, [ConfirmPopUp]);
 
   useEffect(() => {
-    setUserTypeId(AllLogins.data.userId);
+    setUserTypeId(AllLogins.data.userTypeId);
   }, [AllLogins]);
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    const values = {
-      natureOfConfirmation: selectedOption,
+  const handleSubmit = async (values) => {
+    const submitValues = {
+      natureOfConfirmation: values.natureOfConfirmation,
       remarks: remarks,
       confirmedBy: userTypeId,
       id: ConfirmPopUp?.[1],
       confirmedDate: new Date().toISOString().split("T")[0],
     };
-    
+
     try {
-      const response = await confirmCase(values);
+      const response = await confirmCase(submitValues);
 
       if (response && response.message) {
         messageApi.success(response.message);
         setTimeout(() => {
           closeVIewConfirmPopUp();
-          window.location.href = "/dashboard";
         }, 1000);
       } else {
         messageApi.error("Confirmation failed");
@@ -55,13 +59,21 @@ const ConfirmCasePopup = ({
     }
   };
 
+  const confirmationOptions = [
+    "Clinical only",
+    "Clinical and epidemiological",
+    "Clinical and bacteriological",
+    "Clinical and serological",
+    "Clinical, bacteriological and serological",
+    "Clinical and direct microscopy",
+  ];
+
   return (
     <>
       {contextHolder}
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-[#080809]/80 z-50">
-          <div className="bg-white w-[400px] h-[500px] rounded-[8px] shadow-sm flex flex-col">
-            {/* Header Section */}
+          <div className="bg-white w-[400px] min-h-[500px] max-h-[95vh] overflow-y-auto  rounded-[8px] shadow-sm flex flex-col">
             <div className="h-[56px] px-[16px] py-[8px] flex items-center justify-between border-b border-[#E2E5E9]">
               <h1 className="w-full text-center text-[24px] font-medium">
                 Confirm Case
@@ -78,57 +90,62 @@ const ConfirmCasePopup = ({
               <h2 className="w-full text-left text-[20px] font-medium">
                 Nature of Confirmation
               </h2>
-              <form onSubmit={handleSubmit}>
-                {/* Radio Options */}
-                <div className="space-y-2">
-                  {[
-                    "Clinical only",
-                    "Clinical and epidemiological",
-                    "Clinical and bacteriological",
-                    "Clinical and serological",
-                    "Clinical, bacteriological and serological",
-                    "Clinical and direct microscopy",
-                  ].map((option, index) => (
-                    <div key={index} className="flex items-center">
-                      <input
-                        type="radio"
-                        id={`option-${index}`}
-                        name="diagnosis"
-                        value={option}
-                        checked={selectedOption === option}
-                        onChange={(e) => setSelectedOption(e.target.value)}
-                        className="form-radio h-4 w-4 text-blue-600"
-                      />
-                      <label
-                        htmlFor={`option-${index}`}
-                        className="ml-2 text-gray-700"
-                      >
-                        {option}
-                      </label>
+
+              <Formik
+                initialValues={{
+                  natureOfConfirmation: "",
+                }}
+                validationSchema={validationSchema}
+                onSubmit={handleSubmit}
+              >
+                {({ errors, touched }) => (
+                  <Form>
+                    <div className="space-y-2">
+                      {confirmationOptions.map((option, index) => (
+                        <div key={index} className="flex items-center">
+                          <Field
+                            type="radio"
+                            id={`option-${index}`}
+                            name="natureOfConfirmation"
+                            value={option}
+                            className="form-radio h-4 w-4 text-blue-600"
+                          />
+                          <label
+                            htmlFor={`option-${index}`}
+                            className="ml-2 text-gray-700"
+                          >
+                            {option}
+                          </label>
+                        </div>
+                      ))}
+                      {errors.natureOfConfirmation &&
+                        touched.natureOfConfirmation && (
+                          <div className="text-red-500 text-sm mt-1">
+                            {errors.natureOfConfirmation}
+                          </div>
+                        )}
                     </div>
-                  ))}
-                </div>
 
-                {/* Remarks Section */}
-                <div className="mt-6">
-                  <h2 className="text-[20px] font-medium text-gray-800 mb-2">
-                    Remarks
-                  </h2>
-                  <textarea
-                    name="phiRemarks"
-                    className="w-full px-4 py-2 bg-gray-200 rounded-md focus:outline-none"
-                    onChange={(e) => setRemarks(e.target.value)}
-                  />
-                </div>
+                    <div className="mt-6">
+                      <h2 className="text-[20px] font-medium text-gray-800 mb-2">
+                        Remarks
+                      </h2>
+                      <textarea
+                        name="phiRemarks"
+                        className="w-full px-4 py-2 bg-gray-200 rounded-md focus:outline-none"
+                        onChange={(e) => setRemarks(e.target.value)}
+                      />
+                    </div>
 
-                {/* Submit Button */}
-                <button
-                  type="submit"
-                  className="w-full bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition duration-200 mt-3"
-                >
-                  Confirm
-                </button>
-              </form>
+                    <button
+                      type="submit"
+                      className="w-full bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition duration-200 mt-3"
+                    >
+                      Confirm
+                    </button>
+                  </Form>
+                )}
+              </Formik>
             </div>
           </div>
         </div>
